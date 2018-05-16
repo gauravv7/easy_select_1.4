4498515bc28fda3c7c680a706f86dd24af8b1324
gaurav arora <gaurav.arora0403@gmail.com>
2018-05-16 12:18:03 +0530

    issue-44: adding images to options - easy_select2 init call customized to JQuery plugin. - callback to render custom html added via JQuery plugin init call - backward compatible preserved

--- sampleproject/sampleproject/static/js/custom_select2.js
+++ sampleproject/sampleproject/static/js/custom_select2.js
@@ -1,19 +1,13 @@
-
-
+// follow this script to easy_select/init.js
 (function ($) {
     "use strict";
 
-    function formatState (state) {
-      console.log('being called');
-      var baseUrl = "http://lorempixel.com/4/4/";
-      var $state = $(
-        '<span><img src="' + baseUrl + '" class="img-flag" /> ' + state.text + '</span>'
-      );
-      return $state;
-    };
-    function redisplay_select2($el){
-        var obj = $el.data();
-        obj.templateSelection = formatState;
+    /**
+     * passing the current element to initialize and the options to initialize with
+     * @param $el - Jquery object to initialize with select2
+     * @param obj - select2 constructor options
+     */
+    function redisplay_select2($el, obj){
 
         if($("#" + $el.attr('id')).hasClass("select2-hidden-accessible")){
             $("#" + $el.attr('id')).select2('destroy').select2(obj);
@@ -22,19 +16,56 @@
         }
 
     }
-    function add_select2_handlers(){
+
+    /**
+     * core function call for easy_select2
+     * @param options - select2 constructor properties
+     */
+    function add_select2_handlers(options){
 
 
         $('div.field-easy-select2:not([id*="__prefix__"])').each(function(){
+            // taking data-* for select2 constructor properties for backward compatibility
             var obj = $(this).data();
-            obj.templateSelection = formatState;
-            redisplay_select2($(this));
+
+            // merging the options and data properties, modifying the first
+            // NOTE: obj properties will be overwritten by options
+            // https://api.jquery.com/jquery.extend/
+            $.extend(obj, options);
+
+            redisplay_select2($(this), obj);
             $("#" + $(this).attr('id')).on('DOMNodeInserted', function(){
                 $(this).select2('destroy').select2(obj);
             })
         });
     }
-    $(function(){
-        add_select2_handlers();
-    });
+
+    /**
+     * JQuery plugin for django-easy-select2
+     * @param options - object containing select2 constructor properties
+     */
+    $.fn.easy_select = function(options){
+        add_select2_handlers(options);
+    };
+
 }(jQuery || django.jQuery));
+
+
+// sample property callback for select2 constructor argument
+function formatState (state) {
+  console.log('--being called--');
+  var baseUrl = "http://lorempixel.com/4/4/";
+  var $state = $(
+    '<span><img src="' + baseUrl + '" class="img-flag" /> ' + state.text + '</span>'
+  );
+  return $state;
+};
+
+/**
+ * this call can be in a separate file, making the file optional to include or override for properties
+ */
+$(document).ready(function(){
+    $('body').easy_select({
+        templateSelection: formatState
+    });
+})
